18ffa770d67d3ef38a514067e720bf9e1afc69a1
SN15 <sn15@SN15s-Mac-mini.local>
2025-10-11 16:27:40 +0530

    WIP: recent local changes before merging main

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+plugins: [react()],
+server: {
+https: {
+key: fs.readFileSync(path.resolve('/etc/letsencrypt/live/snfifteen.com/privkey.pem')),
+cert: fs.readFileSync(path.resolve('/etc/letsencrypt/live/snfifteen.com/fullchain.pem')),
+},
+host: '0.0.0.0', // Listen on all network interfaces
+port: 443,
+allowedHosts: ['https://www.snfifteen.com',],
+  //allowedHosts: ['snfifteen.com', 'www.snfifteen.com'],
+proxy: {
+'/api': {
+target: 'http://192.168.29.15:5000',
+changeOrigin: true,
+rewrite: (path) => path.replace(/^\/api/, ''),
+},
+},
+}
 });
+
